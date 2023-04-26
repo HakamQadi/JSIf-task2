71aad6ec786b81d80be0dbd7da531d33f4aebf1d
HakamQadi <hakam.h.qadi@gmail.com>
2023-04-26 15:49:32 +0300

    done

---
+++ main.js
@@ -0,0 +1,47 @@
+// Ex.1
+var age = 27;
+if (age > 18) {
+  console.log("you are an adult");
+}
+
+console.log("**************");
+// Ex.2
+var score = 45;
+if (score <= 50) {
+  console.log("You failed the test");
+}
+
+console.log("**************");
+// Ex.3
+var name = "john";
+if (name.includes("john")) {
+  console.log("Hello," + name);
+}
+
+console.log("**************");
+// Ex.4
+var day = "monday";
+if (day != "saturday" && day != "sunday") {
+  console.log("It's a weekday");
+}
+
+console.log("**************");
+// Ex.5
+var num = 4;
+if (num % 2 == 0) {
+  console.log("The number is even");
+}
+
+console.log("**************");
+// Ex.6
+var char="a"
+if (char.length==1) {
+ console.log("It's a letter")   
+}
+
+console.log("**************");
+// Ex.7
+var list=[1,2,3]
+if (Array.isArray(list)) {
+    console.log("It's an array")
+}
